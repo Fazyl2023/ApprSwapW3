f8e43b841a7ffde5637c65b97b103e71edf386f7
Fazyl2023 <khusaenov.fazyl@mail.ru>
2025-05-05 11:45:40 +0300

    clear data

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 rpc = 'https://arb1.arbitrum.io/rpc'
-private_key = 'edbc325aee9a4488f37bda3e74de2b63d9247ee687bd2518cf56f78aa1a1d17b'
+private_key = 'your private key'
 
-my_contract_address = '0xBa2c5150e175F2C33a44037c49b7369df4273985' 
+my_contract_address = 'your public address' 
 usdc_address = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
 
 eth_address = '0x9dDA6Ef3D919c9bC8885D5560999A3640431e8e6'
